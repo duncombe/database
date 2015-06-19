26843c1cefc85a19cd0ccac3ed6feb5313b06c8a
Chris Duncombe Rae <christopher.duncombe.rae@gmail.com>
2015-06-19 14:31:14 +0200

    Updating make_catalog notes.md register.sh

---
+++ notes.md
@@ -0,0 +1,12 @@
+
+for a filename     
+
+-	calculate a shasum  
+-	check if the shasum is in the database  
+-	if not create the database entry  
+-	calculate a uuid  
+-	print the shasum, the uuid, and the original filename   
+
+
+
+
--- register.sh
+++ register.sh
@@ -1,6 +1,17 @@
-uuid=`uuidgen -r`
-echo "# Test database catalog"
-echo "# SHASUM	UUID	Filename"
-cat "$@" | shasum -a 384 | sed 's/-/'$uuid' /' 
 
+# this little script creates a uuid, calculates a checksum and prints them
+# out. 
 
+chksum=`cat "$@" | shasum -a 384 | cut -f1 -d\ `
+dir=`echo $chksum | cut -b-2`
+file=`echo $chksum | cut -b3-`
+
+if [ -f $dir/$file ]; then 
+	echo \"$@\" already registered
+else
+	mkdir database/$dir
+	cp -i "$@" $dir/$file
+
+	uuid=`uuidgen -r`
+	echo $chksum  $uuid  $@ >> catalog
+fi
