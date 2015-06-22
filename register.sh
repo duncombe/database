2ddc9c2989ff529f9b5420958503a5cdb6b2a0a0
Chris Duncombe Rae <christopher.duncombe.rae@gmail.com>
2015-06-22 16:20:43 +0200

    This is not working\!

--- register.sh
+++ register.sh
@@ -1,8 +1,9 @@
-
+#! /bin/bash
 # this little script creates a uuid, calculates a checksum and prints them
 # out. 
 
-database=database
+catalog=${CATALOG:-catalog}
+database=${DATABASE:-database}
 
 chksum=`cat "$@" | shasum -a 384 | cut -f1 -d\ `
 dir=`echo $chksum | cut -b-2`
@@ -10,10 +11,18 @@ file=`echo $chksum | cut -b3-`
 
 if [ ! -f $database/$dir/$file ]; then 
 	mkdir -p $database/$dir
-	cp "$@" $database/$dir/$file
+	cp -i "$@" $database/$dir/$file
+ 	echo Copying in $file 1>&2
 fi
 
-# if ! grep "$@" catalog; then 
+if [ ! -f $catalog ]; then
+	touch $catalog
+# 	echo Forced to create $catalog 1>&2
+fi
+
+if ( ! grep "$@" $catalog > /dev/null ) ; then 
 	uuid=`uuidgen -r`
 	echo  "$chksum  $uuid "
-# fi
+# else
+# 	echo \"$@\" already in database 1>&2
+fi
