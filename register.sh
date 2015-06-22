850fb744b5fc08ab564fea207d55f3389736e41a
Chris Duncombe Rae <christopher.duncombe.rae@gmail.com>
2015-06-22 13:04:21 +0200

    Updating .gitignore check_db make_catalog register.sh

--- register.sh
+++ register.sh
@@ -2,16 +2,18 @@
 # this little script creates a uuid, calculates a checksum and prints them
 # out. 
 
+database=database
+
 chksum=`cat "$@" | shasum -a 384 | cut -f1 -d\ `
 dir=`echo $chksum | cut -b-2`
 file=`echo $chksum | cut -b3-`
 
-if [ -f $dir/$file ]; then 
-	echo \"$@\" already registered
-else
-	mkdir database/$dir
-	cp -i "$@" $dir/$file
-
-	uuid=`uuidgen -r`
-	echo $chksum  $uuid  $@ >> catalog
+if [ ! -f $database/$dir/$file ]; then 
+	mkdir -p $database/$dir
+	cp "$@" $database/$dir/$file
 fi
+
+# if ! grep "$@" catalog; then 
+	uuid=`uuidgen -r`
+	echo  "$chksum  $uuid "
+# fi
