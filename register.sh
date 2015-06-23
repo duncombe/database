e22083ae07e0d95b64616859847520923d449a33
Chris Duncombe Rae <christopher.duncombe.rae@gmail.com>
2015-06-23 12:07:53 +0200

    Updating 5 files

--- register.sh
+++ register.sh
@@ -4,10 +4,13 @@
 
 catalog=${CATALOG:-catalog}
 database=${DATABASE:-database}
+srcdir=${SRCDIR:-srcdir}
+srcfile="${@}"
 
-chksum=`cat "$@" | shasum -a 384 | cut -f1 -d\ `
+chksum=`cat "$srcfile" | shasum -a 384 | cut -f1 -d\ `
 dir=`echo $chksum | cut -b-2`
 file=`echo $chksum | cut -b3-`
+filepath=`echo ${srcfile#$srcdir}`
 
 if [ ! -f $database/$dir/$file ]; then 
 	mkdir -p $database/$dir
@@ -17,12 +20,12 @@ fi
 
 if [ ! -f $catalog ]; then
 	touch $catalog
-# 	echo Forced to create $catalog 1>&2
+ 	echo Forced to create $catalog 1>&2
 fi
 
-if ( ! grep "$@" $catalog > /dev/null ) ; then 
+if ( ! grep "$filepath" $catalog > /dev/null ) ; then 
 	uuid=`uuidgen -r`
-	echo  "$chksum  $uuid "
-# else
-# 	echo \"$@\" already in database 1>&2
+	echo  "$chksum  $uuid  $filepath" >> ${catalog}
+else
+ 	echo \"$@\" already in database 1>&2
 fi
