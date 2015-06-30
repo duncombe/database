e13ed89c25d942668fb8e87eb8154434b601f37d
Chris Duncombe Rae <christopher.duncombe.rae@gmail.com>
2015-06-30 13:34:45 +0200

    Updating register.py requirements.txt

--- register.py
+++ register.py
@@ -1,9 +1,13 @@
 
 import hashlib
+import uuid
+import MySQLdb as mbd
+
 m=hashlib.sha384()
 m.update("Nobody inspects")
 m.update(" the spammish repetition.\n")
 print m.hexdigest()
 
-m=hashlib.sha384("Nobody inspects the spammish repetition.\n")
-print m.hexdigest()
+UUID= uuid.uuid4()
+
+print UUID
