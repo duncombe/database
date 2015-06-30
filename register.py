57d0016e44e2ae593b63594b6779d83a90c7cd69
Chris Duncombe Rae <christopher.duncombe.rae@gmail.com>
2015-06-30 13:57:46 +0200

    Updating register.py

--- register.py
+++ register.py
@@ -3,11 +3,21 @@ import hashlib
 import uuid
 import MySQLdb as mbd
 
+
+
 m=hashlib.sha384()
-m.update("Nobody inspects")
-m.update(" the spammish repetition.\n")
+# m.update("Nobody inspects")
+# m.update(" the spammish repetition.\n")
+
+UUID=uuid.uuid4()
+
+
+fh=open("CTD/db_CTD_data.mdf")
+
+for chunk in fh:
+	m.update(chunk)
+
 print m.hexdigest()
-
-UUID= uuid.uuid4()
-
 print UUID
+
+
