33b67ee741d47b6ddb151962bab3cbf1f875bfed
Chris Duncombe Rae <christopher.duncombe.rae@gmail.com>
2015-08-27 08:21:02 +0200

    Consolidating workflow

---
+++ INGEST/register.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+# this little script creates a uuid, calculates a checksum and prints them
+# out. 
+
+catalog=${CATALOG:-catalog}
+database=${DATABASE:-database}
+srcdir=${SRCDIR:-srcdir}
+srcfile="${@}"
+
+chksum=`cat "$srcfile" | shasum -a 384 | cut -f1 -d\ `
+dir=`echo $chksum | cut -b-2`
+file=`echo $chksum | cut -b3-`
+filepath=`echo ${srcfile#$srcdir}`
+
+if [ ! -f $database/$dir/$file ]; then 
+	mkdir -p $database/$dir
+	cp -i "$@" $database/$dir/$file
+ 	echo Copying in $srcfile 1>&2
+fi
+
+if [ ! -f $catalog ]; then
+	touch $catalog
+ 	echo Forced to create $catalog 1>&2
+fi
+
+if ( ! grep "$filepath" $catalog > /dev/null ) ; then 
+	uuid=`uuidgen -r`
+	echo  "$chksum  $uuid  $filepath" >> ${catalog}
+else
+ 	echo \"$@\" already in database 1>&2
+fi
