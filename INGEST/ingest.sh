bd3457fbf285571d6b9fa056440b64036cb08651
Chris Duncombe Rae <christopher.duncombe.rae@gmail.com>
2015-10-27 13:43:19 +0200

    Updating INGEST/ingest.sh

--- INGEST/ingest.sh
+++ INGEST/ingest.sh
@@ -9,8 +9,22 @@ export CATALOG="/DATA/PUBLICDATA/pub/manifest.txt"
 export DATABASE="/DATA/PUBLICDATA/pub/.DATABASE"
 export LOGFILE="/DATA/PUBLICDATA/pub/ingest.log"
 export ACCESSION_DIR="/DATA/PUBLICDATA/pub/DATA/ACCESSION/"
+export SOURCE_DIR="${1:?}"
 
-./make_catalog "${1:?}" | tee -a ${LOGFILE}
+[ -d "${SOURCE_DIR}" ] || { echo ${SOURCE_DIR} is not accessible ; exit 1 ; } 
+[ -x "${SOURCE_DIR}" ] || { echo ${SOURCE_DIR} is not searchable ; exit 2 ; } 
 
-./create_linked_data ${ACCESSION_DIR}  | tee -a ${LOGFILE}
+[ -e ${LOGFILE} ] || touch ${LOGFILE}
+
+if [ -w ${LOGFILE} ] ; then 
+
+
+	./make_catalog "${SOURCE_DIR}" | tee -a ${LOGFILE}
+
+	./create_linked_data ${ACCESSION_DIR}  | tee -a ${LOGFILE}
+
+else
+	echo ${LOGFILE} is not writable
+	exit 3
+fi
 
