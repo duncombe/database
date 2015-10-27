6b925a0085f03a1a0012ffdbd1795d65d75cdf2e
Christopher Duncombe Rae <christopher.duncombe.rae@gmail.com>
2015-10-27 11:17:53 +0200

    Updating INGEST/ingest.sh

--- INGEST/ingest.sh
+++ INGEST/ingest.sh
@@ -8,8 +8,9 @@
 export CATALOG="/DATA/PUBLICDATA/pub/manifest.txt"
 export DATABASE="/DATA/PUBLICDATA/pub/.DATABASE"
 export LOGFILE="/DATA/PUBLICDATA/pub/ingest.log"
+export ACCESSION_DIR="/DATA/PUBLICDATA/pub/DATA/ACCESSION/"
 
 ./make_catalog "${1:?}" | tee -a ${LOGFILE}
 
-./create_linked_data /DATA/PUBLICDATA/pub/DATA/ACCESSION/ | tee -a ${LOGFILE}
+./create_linked_data ${ACCESSION_DIR}  | tee -a ${LOGFILE}
 
