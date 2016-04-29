e8aa2b3e14ba66bb2b133c222152eeea3e126240
Chris Duncombe Rae <christopher.duncombe.rae@gmail.com>
2016-04-29 05:02:41 +0200

    Updating bin/create_linked_data bin/ingest.sh

--- bin/ingest.sh
+++ bin/ingest.sh
@@ -4,6 +4,26 @@
 # register.sh
 # create_linked_data
 
+# We are changing the format of the WAF.
+# It was
+# DATA/
+#      ACCESSION/
+#                {UUID}/
+#                       {original-data-folder}/
+#
+# We want this to become
+# DATA/
+#      ACCESSION/
+#                {UUID}/
+#                       ABOUT/
+#                       0-DATA/{original-data-folder}/
+#                       1-DATA/{modified-data-folder}/
+# 
+# So we have to make allowance for:
+#     1. an additional level of linking
+#     2. specifying what version of the data we are writing
+#            
+
 # TODO: test for valid user:group
 
 # set the base directory for the ingest code
@@ -24,7 +44,12 @@ if [ -w ${LOGFILE} ] ; then
 
 	${INGEST_HOME}/make_catalog "${SOURCE_DIR}" | tee -a ${LOGFILE}
 
-	${INGEST_HOME}/create_linked_data ${ACCESSION_DIR}  | tee -a ${LOGFILE}
+# test the version of the data we are writing
+	i=0
+	while [ -e ${ACCESSION_DIR}/${DATAVERSION} ]; do i=$((i+1)); done
+	DATAVERSION=${i}-DATA
+
+	${INGEST_HOME}/create_linked_data ${ACCESSION_DIR}/${DATAVERSION}  | tee -a ${LOGFILE}
 
 else
 	echo ${LOGFILE} is not writable
