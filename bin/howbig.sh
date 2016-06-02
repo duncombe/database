5b62b3edfd1ade24779464fa7ffe3f4bd0274435
Chris Duncombe Rae <christopher.duncombe.rae@gmail.com>
2016-06-02 05:28:45 +0200

    Updating bin/howbig.sh

---
+++ bin/howbig.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+# find the size of a database: search for the UUID link to the database and
+# return the disk usage
+# howbig UUID
+# ID=6990ec09-0d4c-47aa-9421-8b06b454422c
+
+ID=$1
+for f in `locate -0 $ID | xargs -0 dirname | grep -v /trash | uniq | grep =$ID$`; do 
+	du -sbL --exclude=trash $f
+done
