df76d9096042bf27bcb31b9e3a3bd40f4b885a78
Chris Duncombe Rae <christopher.duncombe.rae@gmail.com>
2016-07-18 15:18:19 +0200

    Updating bin/fix-ver3-ver4.sh

---
+++ bin/fix-ver3-ver4.sh
@@ -0,0 +1,9 @@
+
+# This fix is to add a level in the hierarchy. 
+# move from this DATA -> accession -> ABOUT
+#                                  -> 0-DATA
+#                                  -> 1-DATA
+# to this DATA -> accession -> ABOUT
+#                           -> DATA -> 0-DATA
+#                                   -> 1-DATA
+# have to 
